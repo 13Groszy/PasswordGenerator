8c279230e203dd5577b13c8534cbc3dc3b803923
Mateusz Daszkiewicz <topdrive@Mini-Mateusz.lan>
2022-03-29 20:31:12 +0100

    Select option, checkboxes.

--- app.js
+++ app.js
@@ -1,26 +1,66 @@
 const value = document.getElementById('passwordLength');
-const password = document.getElementById('returnedPassword')
-const badPassword = document.getElementById('badPassword')
+const password = document.getElementById('returnedPassword');
+const badPassword = document.getElementById('badPassword');
+const selectOption = document.querySelector('select')
+const number = document.querySelector("#numbers")
+const lowCase = document.querySelector("#lowercase")
+const uppCase = document.querySelector("#uppercase")
+const symbol = document.querySelector("#symbols")
 
 createPassword = () => {
 
-    let base = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*";
-    let passwordLength = value.value;
-    let returnedPassword = "";
+    let base = "";
+    let numbers = "0123456789";
+    let lowercase = "abcdefghijklmnopqrstuvwxyz";
+    let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let symbols = "!@#$%^&*";
 
-    if (passwordLength <= 5 || passwordLength >= 41) {
-        badPassword.innerHTML = "Your password need to be between 6 to 40 characters"
-        password.innerHTML = ""
-    }else{
-        badPassword.innerHTML = ""
-        for (let i = 0, charLength = base.length; i < passwordLength; i++) {
-            returnedPassword += base.charAt(Math.floor(Math.random() * charLength))
-        }
-        return password.innerHTML = `Your password: ${returnedPassword}`
+    if (number.checked === true) {
+        base += numbers;
+    }
+    if (lowCase.checked === true) {
+        base += lowercase;
+    }
+    if (uppCase.checked === true) {
+        base += uppercase;
+    }
+    if (symbol.checked === true) {
+        base += symbols;
     }
 
-}
+    // Switch work for one case selected, for mutiple doesn't - TO DO
+    // switch (true) {
+    //     case number.checked === true:
+    //         base = base+numbers;
+    //         console.log(base);
+    //         break;
+    //     case lowCase.checked === true:
+    //         base = base+lowercase;
+    //         console.log(base);
+    //         break;
+    //     case uppCase.checked === true:
+    //         base = base+uppercase;
+    //         console.log(base);
+    //         break;
+    //     case symbol.checked === true:
+    //         base = base+symbols;
+    //         console.log(base);
+    //         break;
+    //     default:
+    //         console.log(base);
+    //         break;
+    // }
 
-test = () =>{
-    console.log()
+    let passwordLength = selectOption.value;
+    let returnedPassword = "";
+
+        if (base === "") {
+            password.innerHTML = ' Select at least one option please'
+        } else {
+            for (let i = 0, charLength = base.length; i < passwordLength; i++) {
+                returnedPassword += base.charAt(Math.floor(Math.random() * charLength))
+            }
+            return password.innerHTML = `Your password: ${returnedPassword}`
+        }
+
 }
